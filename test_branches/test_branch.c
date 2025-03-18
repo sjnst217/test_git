eef299d3b8c331732ea78d640b01d4512f553cf6
sjnst217 <acsj217@gmail.com>
2025-03-18 17:38:49 +0900

    복원

--- test_branches/test_branch.c
+++ test_branches/test_branch.c
@@ -5,6 +5,33 @@ void test_branch()
     printf("I will make new branch\n");
 }
 
+void test_branch2()
+{
+    printf("I will mke second branch\n");
+}
+
+void modi2()
+{
+    printf("check bran2");
+}
+
+void test_branch2()
+{
+    printf("I will mke second branch\n");
+}
+
+void modi2()
+{
+    printf("check bran2");
+}
+
+void modi_any()
+{
+    int a = 1;
+
+    printf("%d\n", a);
+}
+
 // 이건 bran1이고
 
 int main()
