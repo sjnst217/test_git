4aa9464bc635db388a43d65b8ce2f9b496cd9420
sjnst217 <acsj217@gmail.com>
2025-03-19 14:29:49 +0900

    사칙연산 프로그램을 위한 기초 작성, 이후 각 개발자들이 서로 다른 branch에서 개발 후 merge하고자 함

---
+++ test_git/test_merge.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// 사직연산 함수를 만드는데, 개발자1은 덧셈, 뺄셈, 개발자2는 곱셈, 나눗셈, 개발자 3은 최대공약수를 만든다고 가정.
+// 각각 다른 branch로 합친다고 하자.
+
+// 개발자 1
+double add(double a, double b);
+double sub(double a, double b);
+
+// 개발자 2
+double mul(double a, double b);
+double div(double a, double b);
+
+// 개발자 3
+double gcd(double a, double b);
+
+int main()
+{
+
+
+    return 0;
+}
