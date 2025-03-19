5f09d5a71e5730b4a1b8f41a7d3743da33ba0896
sjnst217 <acsj217@gmail.com>
2025-03-19 15:05:13 +0900

    개발자3은 개발 완료

--- test_git/test_merge.c
+++ test_git/test_merge.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 // 사직연산 함수를 만드는데, 개발자1은 덧셈, 뺄셈, 개발자2는 곱셈, 나눗셈, 개발자 3은 최대공약수를 만든다고 가정.
 // 각각 다른 branch로 합친다고 하자.
@@ -11,14 +9,31 @@ double sub(double a, double b);
 
 // 개발자 2
 double mul(double a, double b);
-double div(double a, double b);
+double divi(double a, double b);
 
 // 개발자 3
-double gcd(double a, double b);
+int gcd(int a, int b);
+
+int gcd(int a, int b)
+{
+    if(b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return  gcd(b, (a % b));
+    }
+}
 
 int main()
 {
+    double a, b = 0.0;
 
+    a = 14;
+    b = 6;
+
+    printf("gcd(%d, %d) = %d", (int)a, (int)b, gcd((int)a, (int)b));
 
     return 0;
 }
