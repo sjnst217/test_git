33b82a0e1ec21af3793595abeaae22df0e77fe06
sjnst217 <acsj217@gmail.com>
2025-03-19 09:35:16 +0900

    for check checkout --file.txt

--- test_branches/test_branch.c
+++ test_branches/test_branch.c
@@ -10,32 +10,9 @@ void test_branch2()
     printf("I will mke second branch\n");
 }
 
-void modi2()
-{
-    printf("check bran2");
-}
-
-void test_branch2()
-{
-    printf("I will mke second branch\n");
-}
-
-void modi2()
-{
-    printf("check bran2");
-}
-
-void modi_any()
-{
-    int a = 1;
-
-    printf("%d\n", a);
-}
-
 int main()
 {
     test_branch();
-    modi2();
     
     return 0;
 }
