53e7466511cd05056cc32470d40c16f60d3ff00c
sjnst217 <acsj217@gmail.com>
2025-03-19 14:57:14 +0900

    개발자2의 곱셈함수 완료

--- test_git/test_merge.c
+++ test_git/test_merge.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 // 사직연산 함수를 만드는데, 개발자1은 덧셈, 뺄셈, 개발자2는 곱셈, 나눗셈, 개발자 3은 최대공약수를 만든다고 가정.
 // 각각 다른 branch로 합친다고 하자.
@@ -11,10 +9,19 @@ double sub(double a, double b);
 
 // 개발자 2
 double mul(double a, double b);
-double div(double a, double b);
+double divi(double a, double b);
 
 // 개발자 3
 double gcd(double a, double b);
+
+double mul(double a, double b)
+{
+    double mul_res = 0.0;
+
+    mul_res = a * b;
+
+    return mul_res;
+}
 
 int main()
 {
