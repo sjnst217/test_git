1594b7e691a44e66b3e7ea5e8761f6c970e661a1
sjnst217 <acsj217@gmail.com>
2025-03-19 14:52:04 +0900

    실행 안되는 부분 삭제 완료

--- test_git/test_merge.c
+++ test_git/test_merge.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 // 사직연산 함수를 만드는데, 개발자1은 덧셈, 뺄셈, 개발자2는 곱셈, 나눗셈, 개발자 3은 최대공약수를 만든다고 가정.
 // 각각 다른 branch로 합친다고 하자.
@@ -11,7 +9,7 @@ double sub(double a, double b);
 
 // 개발자 2
 double mul(double a, double b);
-double div(double a, double b);
+double divi(double a, double b);
 
 // 개발자 3
 double gcd(double a, double b);
@@ -23,11 +21,11 @@ int main()
     a = 14;
     b = 6;
 
-    printf("%d + %d = %d", a, b, add(a, b));
-    printf("%d - %d = %d", a, b, sub(a, b));
-    printf("%d * %d = %d", a, b, mul(a, b));
-    printf("%d / %d = %d", a, b, div(a, b));
-    printf("gcd(%d, %d) = %d", a, b, gcd(a, b));
+    printf("%lf + %lf = %lf", a, b, add(a, b));
+    printf("%lf - %lf = %lf", a, b, sub(a, b));
+    printf("%lf * %lf = %lf", a, b, mul(a, b));
+    printf("%lf / %lf = %lf", a, b, div(a, b));
+    printf("gcd(%lf, %lf) = %lf", a, b, gcd(a, b));
 
     return 0;
 }
