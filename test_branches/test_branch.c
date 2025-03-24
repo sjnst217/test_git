64812269ba5a4bda61cdf5e9d646529bd78720bd
DESKTOP-6NGCT5Q\sjchoi <sjchoi@ranix.co.kr>
2025-03-24 15:34:11 +0900

    test_branch

--- test_branches/test_branch.c
+++ test_branches/test_branch.c
@@ -5,23 +5,6 @@ void test_branch()
     printf("I will make new branch\n");
 }
 
-void test_branch2()
-{
-    printf("I will mke second branch\n");
-}
-
-void test_stash()
-{
-    printf("test stash\n");
-}
-
-void test_stash2()
-{
-    printf("test stash 2\n");
-}
-
-// 이건 되겠짓짓
-
 int main()
 {
     test_branch();
